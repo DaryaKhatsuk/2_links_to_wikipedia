7f15301ba541a352e1fb75a7b4069d917539e80c
DaryaKhatsuk <DaryaKhatsuk>
2023-03-14 03:41:10 +0300

    commit

--- main.py
+++ main.py
@@ -1,137 +1,68 @@
 import asyncio
 import logging
 import re
-import time
-from urllib.parse import urljoin
 
-from aiohttp import ClientSession
-from fake_useragent import UserAgent
-
-logging.basicConfig(filename='search.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-MAX_DEPTH = 3
-final_title = ''
+import aiohttp
+from bs4 import BeautifulSoup
 
 
-async def get_url_from_title(session, title):
-    url = f'https://ru.wikipedia.org/w/index.php?title=Special:Search&limit=1&offset=0&ns0=1&search={title}'
-    html = await fetch(session, url)
-    links = re.findall(r'<a href="(/wiki/[^"]+)"', html)
-    if links:
-        return 'https://ru.wikipedia.org' + links[0]
-    else:
-        return None
-
-
-async def fetch(session, url):
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-    async with session.get(url, headers=headers) as response:
-        return await response.text()
-
-
-async def get_links(session, url):
-    html = await fetch(session, url)
-    links = re.findall(r'<a href="([^"]+)"[^>]*>(.*?)</a>', html)
-    links = [(urljoin(url, link), title) for link, title in links if 'wikipedia.org' in link]
-    return links
-
-
-async def search_links(session, current_url, target_title, path, depth=3, visited=None):
+async def search_links(session, current_url, target_url, target_title, path, depth=3, visited=None):
     if visited is None:
         visited = set()
-
-    if depth < 0:
-        return None
-
-    if current_url in visited:
-        return None
-
     visited.add(current_url)
+    if depth == 0:
+        return None
+    async with session.get(current_url) as response:
+        text = await response.text()
+        soup = BeautifulSoup(text, 'html.parser')
+        body_content = soup.find('div', id='bodyContent', class_='vector-body')
+        print(body_content)
+        if not body_content:
+            return None
+        match = body_content.find('h1', id='firstHeading', class_='firstHeading')
+        print(match)
+        if match and match.get_text().strip() == target_title:
+            return path + [(match.get_text(), current_url)]
+        links = body_content.find_all('a', href=re.compile(r'^/wiki/[^:]+$'))
+        for link in links:
+            next_url = f"https://en.wikipedia.org{link['href']}"
+            if next_url not in visited:
+                next_path = await search_links(session, next_url, target_url, target_title,
+                                               path + [(link.get_text(), next_url)], depth - 1, visited)
+                if next_path is not None:
+                    return next_path
+        return None
 
-    html = await fetch(session, current_url)
-    title = re.search(r'<firstHeading>(.*?) - Wikipedia</firstHeading>', html).group(1)
-    print(html, title)
-    if title == target_title:
-        return path + [(f'"{title}"', current_url)]
 
-    links = await get_links(session, current_url)
-    for link, link_title in links:
-        logging.info(f'Visiting {link}')
-        subpath = await search_links(session, link, target_title, path + [(f'"{link_title}"', link)], depth=depth - 1,
-                                     visited=visited)
-        if subpath is not None:
-            return subpath
-
-    return None
-
-
-# async def search_links(session, current_url, target_url, path, visited_urls, depth=3):
-#     target_title = re.search(r'/([^/]+)$', link)
-#     if depth < 0:
-#         return None
-#     if current_url == target_url:
-#         return path
-#
-#     if current_url in visited_urls:
-#         return None
-#
-#     visited_urls.add(current_url)
-#
-#     links = await get_links(session, current_url)
-#
-#     for link in links:
-#         if 'wikipedia.org' not in link:
-#             continue
-#
-#         if link in visited_urls:
-#             continue
-#
-#         logging.info(f'Visiting {link}')
-#         new_path = path + [(link, '')]
-#
-#         html = await fetch(session, link)
-#         soup = BeautifulSoup(html, 'html.parser')
-#         title_tag = soup.find('h1', {'class': 'firstHeading'})
-#         if title_tag is None:
-#             continue
-#         title = title_tag.text.replace(' ', '_')
-#         print(title_tag)
-#         title_compar = re.search(r'/([^/]+)$', link)
-#         print(title_compar)
-#         if link == target_url and (title_compar == title):
-#             return new_path
-#
-#         if title.lower() in {'error', 'not found', 'page not found', '404 not found', 'page does not exist'}:
-#             continue
-#
-#         subpath = await search_links(session, link, target_url, new_path, visited_urls, depth=depth-1)
-#         if subpath is not None:
-#             sentence, next_link = subpath[0]
-#             subpath[0] = (sentence, link)
-#             return new_path + subpath[1:]
-#
-#     return None
+async def find_path(start_url, target_url, target_title, depth=3):
+    async with aiohttp.ClientSession() as session:
+        path = await search_links(session, start_url, target_url, target_title, [], depth=depth)
+        return path
 
 
 async def main():
-    start_url = input('Enter start URL: ')
-    target_url = input('Enter target URL: ')
-    async with ClientSession() as session:
-        target_url = await get_url_from_title(session, target_url)
-        if target_url is None:
-            print(f"Couldn't find a page with title '{target_url}'.")
-        else:
-            path = await search_links(session, start_url, target_url, depth=3)
-            if path is not None:
-                for i, (sentence, link) in enumerate(path):
-                    print(f'{i + 1}. {sentence} ({link})')
+    start_url = 'https://en.wikipedia.org/wiki/Xbox_360_S'
+    target_url = 'https://ru.wikipedia.org/wiki/Nintendo_3DS'
+    target_title = str(re.search(r'/([^/]+)$', target_url)).replace('_', ' ')
+    path = await find_path(start_url, target_url, target_title, depth=3)
+    if path:
+        print("Path:")
+        for i, (url, text) in enumerate(path):
+            print(f"{i + 1}------------------------")
+            match = re.search(f'<a href="({target_url})"[^>]*>', text)
+            if match:
+                link_text = match.group(0)
+                start_index = text.rindex(">", 0, match.start()) + 1
+                end_index = text.index("</a>", match.end())
+                link_text += text[start_index:end_index]
+                print(link_text)
             else:
-                print('No path found')
+                print(url)
+    else:
+        print("No path found")
 
 
 if __name__ == '__main__':
-    x = time.time()
+    logging.basicConfig(filename='search.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('Starting search')
     asyncio.run(main())
-    y = time.time()
-    print(y - x)
